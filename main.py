7513ad06c754865881fd0c0e357b736b92b43bbe
musik0110 <115767955+musik0110@users.noreply.github.com>
2023-10-24 10:03:20 +0600

    add unittest

--- main.py
+++ main.py
@@ -29,3 +29,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+import unittest
+
+class TestGame(unittest.TestCase):
+    def test_game_initialization(self):
+        game = Game()
+        self.assertTrue(1 <= game.secret_number <= 100)
+        self.assertEqual(game.attempts, 0)
+
+    def test_guess_lower(self):
+        game = Game()
+        game.secret_number = 50
+        result = game.guess(30)
+        self.assertEqual(result, "Загаданное число больше.")
+
+    def test_guess_higher(self):
+        game = Game()
+        game.secret_number = 50
+        result = game.guess(50070)
+        self.assertEqual(result, "Загаданное число меньше.")
+
+    def test_guess_correct(self):
+        game = Game()
+        game.secret_number = 42
+        result = game.guess(42)
+        self.assertEqual(result, f"Поздравляю! Вы угадали число {game.secret_number} с 1 попытки.")
+
+if __name__ == "__main__":
+    unittest.main()
